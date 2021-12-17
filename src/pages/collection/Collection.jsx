ffe05fe72e11657720afc83d13e47247450c4d67
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2021-12-17 14:29:02 +0100

    Data moved to redux

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -5,12 +5,19 @@ import "./collection.styles.scss";
 import { selectCollection } from "../../redux/shop/shop.selector";
 
 const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>Collection Page</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
+
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
